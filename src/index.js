eac5dc414eb289801306b901694902072935f466
Zertix <3t5i4m8a.bel@gmail.com>
2025-02-15 22:59:13 +0300

    decode from binary to string

--- src/index.js
+++ src/index.js
@@ -38,7 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+
+    for (let i = 0; i < expr.length; i += 10) {
+        let encodedLetter = expr.slice(i, i + 10);
+
+        if (encodedLetter === '**********') {
+            result += ' '; // Пробел
+        } else {
+            let morseCode = encodedLetter
+                .replace(/10/g, '.') // 10 → .
+                .replace(/11/g, '-') // 11 → -
+                .replace(/0/g, '');  // Убираем лишние 0
+
+            result += MORSE_TABLE[morseCode]; // Декодируем букву
+        }
+    }
+    return result;
 }
 
 module.exports = {
